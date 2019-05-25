2d9cc97817a950c792c99f68bcf39c15836d86a4
Kyle Akerstrom <kakerstrom@gmail.com>
2019-05-24 18:18:23 -0600

    Initial commit

---
+++ BatchResize/BatchResizer/BatchResizeForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace BatchResizer
+{
+    partial class BatchResizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectedFolderTxt = new System.Windows.Forms.TextBox();
+            this.chooseFolderBtn = new System.Windows.Forms.Button();
+            this.resizeBtn = new System.Windows.Forms.Button();
+            this.heightNud = new System.Windows.Forms.NumericUpDown();
+            this.widthNud = new System.Windows.Forms.NumericUpDown();
+            this.heightLbl = new System.Windows.Forms.Label();
+            this.widthLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.replaceOriginalsCb = new System.Windows.Forms.CheckBox();
+            this.resizeTypeCb = new System.Windows.Forms.ComboBox();
+            this.currentFileLbl = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthNud)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectedFolderTxt
+            // 
+            this.selectedFolderTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedFolderTxt.Location = new System.Drawing.Point(13, 34);
+            this.selectedFolderTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectedFolderTxt.Name = "selectedFolderTxt";
+            this.selectedFolderTxt.Size = new System.Drawing.Size(353, 26);
+            this.selectedFolderTxt.TabIndex = 0;
+            // 
+            // chooseFolderBtn
+            // 
+            this.chooseFolderBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chooseFolderBtn.Location = new System.Drawing.Point(253, 70);
+            this.chooseFolderBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chooseFolderBtn.Name = "chooseFolderBtn";
+            this.chooseFolderBtn.Size = new System.Drawing.Size(113, 32);
+            this.chooseFolderBtn.TabIndex = 1;
+            this.chooseFolderBtn.Text = "Choose Folder";
+            this.chooseFolderBtn.UseVisualStyleBackColor = true;
+            this.chooseFolderBtn.Click += new System.EventHandler(this.chooseFolderBtn_Click);
+            // 
+            // resizeBtn
+            // 
+            this.resizeBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.resizeBtn.Location = new System.Drawing.Point(72, 331);
+            this.resizeBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resizeBtn.Name = "resizeBtn";
+            this.resizeBtn.Size = new System.Drawing.Size(100, 32);
+            this.resizeBtn.TabIndex = 4;
+            this.resizeBtn.Text = "Resize";
+            this.resizeBtn.UseVisualStyleBackColor = true;
+            this.resizeBtn.Click += new System.EventHandler(this.resizeBtn_Click);
+            // 
+            // heightNud
+            // 
+            this.heightNud.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.heightNud.Location = new System.Drawing.Point(123, 37);
+            this.heightNud.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.heightNud.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.heightNud.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.heightNud.Name = "heightNud";
+            this.heightNud.Size = new System.Drawing.Size(88, 26);
+            this.heightNud.TabIndex = 0;
+            this.heightNud.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.heightNud.Enter += new System.EventHandler(this.Nud_Enter);
+            // 
+            // widthNud
+            // 
+            this.widthNud.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.widthNud.Location = new System.Drawing.Point(123, 77);
+            this.widthNud.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.widthNud.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.widthNud.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.widthNud.Name = "widthNud";
+            this.widthNud.Size = new System.Drawing.Size(88, 26);
+            this.widthNud.TabIndex = 1;
+            this.widthNud.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.widthNud.Enter += new System.EventHandler(this.Nud_Enter);
+            // 
+            // heightLbl
+            // 
+            this.heightLbl.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.heightLbl.Location = new System.Drawing.Point(23, 37);
+            this.heightLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.heightLbl.Name = "heightLbl";
+            this.heightLbl.Size = new System.Drawing.Size(92, 31);
+            this.heightLbl.TabIndex = 5;
+            this.heightLbl.Text = "Max Height";
+            this.heightLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // widthLbl
+            // 
+            this.widthLbl.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.widthLbl.Location = new System.Drawing.Point(23, 77);
+            this.widthLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.widthLbl.Name = "widthLbl";
+            this.widthLbl.Size = new System.Drawing.Size(92, 31);
+            this.widthLbl.TabIndex = 6;
+            this.widthLbl.Text = "Max Width";
+            this.widthLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.replaceOriginalsCb);
+            this.groupBox1.Controls.Add(this.heightLbl);
+            this.groupBox1.Controls.Add(this.widthLbl);
+            this.groupBox1.Controls.Add(this.heightNud);
+            this.groupBox1.Controls.Add(this.widthNud);
+            this.groupBox1.Location = new System.Drawing.Point(72, 161);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 148);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // replaceOriginalsCb
+            // 
+            this.replaceOriginalsCb.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.replaceOriginalsCb.Location = new System.Drawing.Point(50, 118);
+            this.replaceOriginalsCb.Name = "replaceOriginalsCb";
+            this.replaceOriginalsCb.Size = new System.Drawing.Size(134, 24);
+            this.replaceOriginalsCb.TabIndex = 2;
+            this.replaceOriginalsCb.Text = "Replace Originals";
+            this.replaceOriginalsCb.UseVisualStyleBackColor = true;
+            this.replaceOriginalsCb.CheckedChanged += new System.EventHandler(this.replaceOriginalsCb_CheckedChanged);
+            // 
+            // resizeTypeCb
+            // 
+            this.resizeTypeCb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resizeTypeCb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.resizeTypeCb.FormattingEnabled = true;
+            this.resizeTypeCb.Items.AddRange(new object[] {
+            "Shrink to Max Size",
+            "Grow to Min Size",
+            "Set Exact Size",
+            "By Percent"});
+            this.resizeTypeCb.Location = new System.Drawing.Point(72, 127);
+            this.resizeTypeCb.Name = "resizeTypeCb";
+            this.resizeTypeCb.Size = new System.Drawing.Size(235, 28);
+            this.resizeTypeCb.TabIndex = 2;
+            this.resizeTypeCb.SelectedIndexChanged += new System.EventHandler(this.resizeTypeCb_SelectedIndexChanged);
+            // 
+            // currentFileLbl
+            // 
+            this.currentFileLbl.AutoEllipsis = true;
+            this.currentFileLbl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.currentFileLbl.Location = new System.Drawing.Point(0, 377);
+            this.currentFileLbl.Name = "currentFileLbl";
+            this.currentFileLbl.Size = new System.Drawing.Size(379, 25);
+            this.currentFileLbl.TabIndex = 8;
+            this.currentFileLbl.Text = "[show current file]";
+            this.currentFileLbl.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(379, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cancelBtn.Enabled = false;
+            this.cancelBtn.Location = new System.Drawing.Point(207, 331);
+            this.cancelBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(100, 32);
+            this.cancelBtn.TabIndex = 10;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(379, 402);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.currentFileLbl);
+            this.Controls.Add(this.resizeTypeCb);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.resizeBtn);
+            this.Controls.Add(this.chooseFolderBtn);
+            this.Controls.Add(this.selectedFolderTxt);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Batch Image Resizer";
+            ((System.ComponentModel.ISupportInitialize)(this.heightNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.widthNud)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox selectedFolderTxt;
+        private System.Windows.Forms.Button chooseFolderBtn;
+        private System.Windows.Forms.Button resizeBtn;
+        private System.Windows.Forms.NumericUpDown heightNud;
+        private System.Windows.Forms.NumericUpDown widthNud;
+        private System.Windows.Forms.Label heightLbl;
+        private System.Windows.Forms.Label widthLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox resizeTypeCb;
+        private System.Windows.Forms.CheckBox replaceOriginalsCb;
+        private System.Windows.Forms.Label currentFileLbl;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
+
---
+++ BatchResize/BatchResizer/BatchResizeForm.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchResizer
+{
+    public partial class BatchResizeForm : Form
+    {
+        private enum ResizeType
+        {
+            ShrinkToMax = 0, GrowToMin, ResizeExact, ByPercent
+        }
+
+        private IProgress<string> FileResizingProgress;
+        private IProgress<string> FileResizedProgress;
+        private IProgress<string> ErrorProgress;
+
+        private bool errorsOccurred;
+        private bool isCancelled;
+        private DateTime lastStatusUpdate;
+
+        public BatchResizeForm()
+        {
+            InitializeComponent();
+            FileResizingProgress = new Progress<string>(ReportFileResizing);
+            ErrorProgress = new Progress<string>(ReportError);
+            resizeTypeCb.SelectedIndex = 0;
+            currentFileLbl.Text = string.Empty;
+            heightLbl.Text = "Max Height";
+            heightLbl.Text = "Max Width";
+        }
+
+        private void chooseFolderBtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.ShowNewFolderButton = false;
+                fbd.Description = "Choose root folder";
+                if (fbd.ShowDialog() == DialogResult.OK)
+                    selectedFolderTxt.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void resizeBtn_Click(object sender, EventArgs e)
+        {
+            string root = selectedFolderTxt.Text;
+            int height = (int)heightNud.Value;
+            int width = (int)widthNud.Value;
+
+            if (!Directory.Exists(root))
+            {
+                MessageBox.Show("Invalid folder path.");
+                return;
+            }
+
+            if (replaceOriginalsCb.Checked && MessageBox.Show(
+                    "Are you completely, totally sure you trust me not to delete your original files?\n" +
+                    "Should I overwrite your original files with the resized versions?",
+                    "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            ResizeAllImages(root, (ResizeType)resizeTypeCb.SelectedIndex, height, width, replaceOriginalsCb.Checked);
+        }
+
+        private async void ResizeAllImages(string root, ResizeType resizeType, int height, int width, bool replaceOriginal)
+        {
+            isCancelled = false;
+            foreach (Control control in Controls)
+                if (!(control is Label))
+                    control.Enabled = false;
+            cancelBtn.Enabled = true;
+
+            await Task.Run(() => ResizeImagesInFolder(root, resizeType, height, width, replaceOriginal));
+
+            if (isCancelled)
+                MessageBox.Show("Batch resizing was cancelled.");
+            else
+                MessageBox.Show("Done!");
+
+            if(errorsOccurred)
+                MessageBox.Show("Errors occurred while attempting to resize files. You can view the error log in .\\errorlog.txt");
+
+            errorsOccurred = false;
+            isCancelled = false;
+            foreach (Control control in Controls)
+                if (!(control is Label))
+                    control.Enabled = true;
+            selectedFolderTxt.Text = string.Empty;
+            cancelBtn.Enabled = false;
+            currentFileLbl.Text = string.Empty;
+        }
+
+        private void ResizeImagesInFolder(string folderPath, ResizeType resizeType, int width, int height, bool replaceOriginal)
+        {
+            string[] fileTypes = { ".jpg", ".jpeg", ".png", ".bmp" }; //Add support for gifs??
+            foreach (string file in Directory.GetFiles(folderPath))
+            {
+                if (isCancelled) return;
+                bool isValid = false;
+                foreach (string filetype in fileTypes)
+                    if (file.EndsWith(filetype))
+                    {
+                        isValid = true;
+                        break;
+                    }
+                if (isValid)
+                    ResizeImage(file, resizeType, width, height, replaceOriginal);
+            }
+
+            foreach (string folder in Directory.GetDirectories(folderPath))
+                ResizeImagesInFolder(folder, resizeType, width, height, replaceOriginal);
+        }
+
+        private void ResizeImage(string filepath, ResizeType resizeType, int width, int height, bool replaceOriginal)
+        {
+            if (isCancelled) return;
+
+            try
+            {
+                FileResizingProgress.Report("Resizing " + filepath);
+                Bitmap bmp = new Bitmap(filepath);
+                float xScale, yScale;
+
+                switch (resizeType)
+                {
+                    case ResizeType.ShrinkToMax:
+                        if (bmp.PhysicalDimension.Width <= width && bmp.PhysicalDimension.Height <= height)
+                            //Todo: Copy image to folder if necessary
+                            return;
+                        else
+                            xScale = yScale = Math.Min(width / bmp.PhysicalDimension.Width,
+                                height / bmp.PhysicalDimension.Height);
+                        break;
+
+                    case ResizeType.GrowToMin:
+                        if (bmp.PhysicalDimension.Width >= width && bmp.PhysicalDimension.Height >= height)
+                            //Todo: Copy image to folder if necessary
+                            return;
+                        else
+                            xScale = yScale = Math.Max(width / bmp.PhysicalDimension.Width,
+                                height / bmp.PhysicalDimension.Height);
+                        break;
+
+                    case ResizeType.ResizeExact:
+                        if ((int)bmp.PhysicalDimension.Width == width && (int)bmp.PhysicalDimension.Height == height)
+                            //Todo: Copy image to folder if necessary
+                            return;
+                        else
+                        {
+                            xScale = width / bmp.PhysicalDimension.Width;
+                            yScale = height / bmp.PhysicalDimension.Height;
+                        }
+
+                        break;
+
+                    case ResizeType.ByPercent:
+                        xScale = (float)width / 100;
+                        yScale = (float)height / 100;
+                        break;
+
+                    default:
+                        MessageBox.Show("Programmer messed up. Invalid Resize Type.");
+                        return;
+                }
+
+                if (isCancelled) return;
+
+                using (Bitmap smallBmp = new Bitmap(bmp,
+                    new Size((int)(bmp.PhysicalDimension.Width * xScale),
+                        (int)(bmp.PhysicalDimension.Height * yScale))))
+                {
+                    bmp.Dispose();
+                    ImageFormat imgFormat = ImageFormat.Jpeg;
+                    if (filepath.EndsWith(".jpg") || filepath.EndsWith(".jpeg"))
+                        imgFormat = ImageFormat.Jpeg;
+                    else if (filepath.EndsWith(".bmp"))
+                        imgFormat = ImageFormat.Bmp;
+                    else if (filepath.EndsWith(".png"))
+                        imgFormat = ImageFormat.Png;
+
+                    if (isCancelled) return;
+
+                    if (replaceOriginal)
+                    {
+                        File.Delete(filepath);
+                        smallBmp.Save(filepath, ImageFormat.Jpeg);
+                    }
+                    else
+                    {
+                        string prePath = filepath.Substring(0, filepath.LastIndexOf('.'));
+                        string extension = filepath.Substring(filepath.LastIndexOf('.'));
+                        string newFilePath = prePath;
+                        do
+                        {
+                            newFilePath = newFilePath + "_resized";
+                        } while (File.Exists(newFilePath + extension));
+
+                        smallBmp.Save(newFilePath + extension, imgFormat);
+                    }
+                }
+            }
+            catch (AccessViolationException ex)
+            {
+                ErrorProgress.Report($"Access Violation Error in {filepath}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                ErrorProgress.Report($"Error resizing {filepath}: {ex.Message}");
+            }
+        }
+
+        private void replaceOriginalsCb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (replaceOriginalsCb.Checked)
+            {
+                if (MessageBox.Show(
+                        "Are you completely, totally sure you trust me not to delete your original files?\n" +
+                        "Should I overwrite your original files with the resized versions?",
+                        "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.No)
+                    replaceOriginalsCb.Checked = false;
+            }
+        }
+
+        private void ReportFileResizing(string message)
+        {
+            if (DateTime.Now - lastStatusUpdate >= TimeSpan.FromSeconds(1))
+            {
+                currentFileLbl.Text = message;
+                lastStatusUpdate = DateTime.Now;
+            }
+        }
+
+        private void ReportError(string message)
+        {
+            errorsOccurred = true;
+            File.AppendAllText(@".\errorlog.txt", message);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Batch Resizer program version 1.0\n" +
+                             "2018\n\n" +
+                             "Written by Kyle Akerstrom\n" +
+                             "kakerstrom@gmail.com\n\n" +
+                             "Currently supports the following formats:\n" +
+                             ".jpg, .png, .bmp\n\n" +
+                             "I did my best to keep your files safe,\n" +
+                             "but you still use this software at your own risk!\n\n";
+
+            MessageBox.Show(message, "About Batch Resizer", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void resizeTypeCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch ((ResizeType)resizeTypeCb.SelectedIndex)
+            {
+                case ResizeType.ShrinkToMax:
+                    widthLbl.Text = "Max Width";
+                    heightLbl.Text = "Max Height";
+                    break;
+                case ResizeType.GrowToMin:
+                    widthLbl.Text = "Min Width";
+                    heightLbl.Text = "Min Height";
+                    break;
+                case ResizeType.ResizeExact:
+                    widthLbl.Text = "Width";
+                    heightLbl.Text = "Height";
+                    break;
+                case ResizeType.ByPercent:
+                    widthLbl.Text = "% Width";
+                    heightLbl.Text = "% Height";
+                    break;
+            }
+        }
+
+        private void Nud_Enter(object sender, EventArgs e)
+        {
+            ((NumericUpDown)sender).Select(0, ((NumericUpDown)sender).Text.Length);
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            isCancelled = true;
+        }
+    }
+}
